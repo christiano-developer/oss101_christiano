93d1f63f1ab1a797dd8502ecb45ee34b58dc2714
christiano-developer <christiano.developer05@gmail.com>
2024-01-06 17:17:16 +0530

    linter errors fixed in py file

--- contributors/nebulatris/christiano-developer/sourcecodeitdmchallenge6.py
+++ contributors/nebulatris/christiano-developer/sourcecodeitdmchallenge6.py
@@ -1,19 +1,21 @@
 import random as rn
 print("Guess the number!! in  3 guesses, abs int will be handled")
 print("ranging from 0 to 100, endpoints included")
-a = rn.randint(0,100)
+a = rn.randint(0, 100)
 count = 0
 print(a)
+
+
 def check(count):
     b = int(input())
-    
+
     if b > 100:
         return "Out of bound READ!"
     if count > 2:
         print("booo")
-    
+
     if a == b:
-       print("Genius")
+        print("Genius")
     elif b > a:
         print("Lower")
         check(count)
